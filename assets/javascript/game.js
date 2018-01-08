20f0c69cc0ab9a55fb13fd5e95fea6b39a8292f7
scull1916 <kpscully@gmail.com>
2018-01-07 19:27:33 -0500

    small victories: dynamically adding crystal images achieved!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,66 +20,21 @@ console.log(targetNumber);
 // var targetNumber = 50;
 $("#target").text(targetNumber);
 
-// Generate 4 random numbers and assign to each of the gems
-
-// var gem1 = function getRandomIntInclusive(min, max)
-// {
-// 	min=Math.ceil(1);
-// 	max=Math.floor(12);
-// 	return Math.floor(Math.random() * (max - min +1)) + min;
-
-
-// }
-// // $("gem1").text(clickScore)
-
-// console.log(gem1());
-
-// var gem2 = function getRandomIntInclusive(min, max)
-// {
-// 	min=Math.ceil(1);
-// 	max=Math.floor(12);
-// 	return Math.floor(Math.random() * (max - min +1)) + min;
-
-
-// }
-// // $("gem2").text(clickScore)
-// console.log(gem2());
-
-// var gem3 = function getRandomIntInclusive(min, max)
-// {
-// 	min=Math.ceil(1);
-// 	max=Math.floor(12);
-// 	return Math.floor(Math.random() * (max - min +1)) + min;
-
-
-// }
-// // $("gem3").text(clickScore)
-// console.log(gem3());
-
-// var gem4 = function getRandomIntInclusive(min, max)
-// {
-// 	min=Math.ceil(1);
-// 	max=Math.floor(12);
-// 	return Math.floor(Math.random() * (max - min +1)) + min;
-
-
-// }
-// $("gem4").text(clickScore)
-// console.log(gem4());
-
-
 
 
 var counter = 0;
+
+//Create an array to hold the crystal file locations
+
 var images = [
-	'../assets/images/amethyst.png', 
-	'../assets/images/emerald.png', 
-	'../assets/images/ruby.jpg', 
-	'../assets/images/sapphire.png'
+	'../week-4-game/assets/images/amethyst.png', 
+	'../week-4-game/assets/images/emerald.png', 
+	'../week-4-game/assets/images/ruby.jpg', 
+	'../week-4-game/assets/images/sapphire.png'
 	];
 console.log(images);
 
-var crystalValue = [0,0,0,0];
+var crystalValue = [1,1,1,1];
 
 console.log(crystalValue);
 
@@ -91,12 +46,12 @@ for (var i = 0; i < crystalValue.length; i++)
 		min=Math.ceil(1);
 		max=Math.floor(12);
 		return Math.floor(Math.random() * (max - min +1)) + min;
-		
-	crystalValue.push(this);
+
 	};
 
 	console.log(gemValue());
 
+	
 
 }
 
@@ -109,9 +64,9 @@ for (var i = 0; i < crystalValue.length; i++)
 	
 	var imageCrystal = $("<img>");
 
-	// imageCrystal.addclass("crystal-image");
+	imageCrystal.addclass("crystal-image");
 	
-	imageCrystal.attr(images[i]);
+	imageCrystal.attr("src", images[i]);
 
 	imageCrystal.attr("data-crystalvalue",crystalValue[i]);
 
@@ -134,27 +89,7 @@ $(".crystal-image").on("click", function()
 
 
 });
-// $(".crystal-image").on("click", function() {
-// 	var gemClicked = $(this).attr("class");
-// 	console.log(gemClicked);
 
-// 	switch (gemClicked){
-// 		case "gem1":
-// 		counter = counter + gem1();
-
-// 		case "gem2":
-// 		counter = counter + gem2();
-
-// 		case "gem3":
-// 		counter = counter + gem3();
-
-// 		case "gem4":
-// 		counter = counter + gem4();
-
-// 		default:
-// 			counter = 0;
-
-// 	}
 
 // console.log(counter);
 
